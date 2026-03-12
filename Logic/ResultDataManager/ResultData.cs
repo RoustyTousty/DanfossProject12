ed2eb2e3094822f9fd5ba7b81f647382ff88344a
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2026-03-12 20:22:29 +0100

    made result data inherit from HourlyData since ResultData directly extends HourlyData

--- Logic/ResultDataManager/ResultData.cs
+++ Logic/ResultDataManager/ResultData.cs
@@ -1,11 +1,5 @@
-public class ResultData
+public class ResultData : HourlyData
 {
-    public DateTime TimeFrom { get; set; }
-    public DateTime TimeTo { get; set; }
-
-    public double HeatDemandMWh { get; set; }
-    public double ElectricityPriceDKK { get; set; }
-
     public double HeatProductionMWh { get; set; }
     public double ElectricityProductionMWh { get; set; }
     public double ElectricityConsumptionMWh { get; set; }
