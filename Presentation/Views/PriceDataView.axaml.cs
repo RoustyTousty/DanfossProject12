3ef5938935ff57f637dc929923ee7761226f23f7
RoustyTousty <emils.misisons@gmail.com>
2026-04-14 23:12:48 +0200

    Main view & Avalonia setup & home/opti/data page setup

---
+++ Presentation/Views/PriceDataView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HeatOptimization.Presentation.Views;
+
+public partial class PriceDataView : UserControl
+{
+    public PriceDataView()
+    {
+        InitializeComponent();
+    }
+}
