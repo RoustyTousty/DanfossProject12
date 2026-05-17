7367c724a686d14c12a49b8abce7a4cbab2c69e5
Daniel M <migunok12@gmail.com>
2026-05-17 14:01:51 +0200

    File uploading (Update)

--- Presentation/ViewModels/ConfigViewModel.cs
+++ Presentation/ViewModels/ConfigViewModel.cs
@@ -3,6 +3,10 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using Avalonia.Media.Imaging;
 using HeatOptimization.Logic;
+using Avalonia.Platform.Storage;
+using Avalonia.Interactivity;
+using Avalonia.Controls;
+using System.IO;
 
 namespace HeatOptimization.Presentation.ViewModels;
 
@@ -10,10 +14,11 @@ public partial class ConfigViewModel : ViewModelBase
 {
     private AssetService _assetService;
     private OptimizationService _optimizationService;
+    private readonly Func<Task<string?>>? _filePickerService;
 
     public ObservableCollection<ProductionUnit> ActiveUnits { get; set; } = [];
 
-public ObservableCollection<ProductionUnit> DisabledUnits { get; set; } = [];
+    public ObservableCollection<ProductionUnit> DisabledUnits { get; set; } = [];
 
     [ObservableProperty]
     private int maintenanceHours = 30;
@@ -24,17 +29,46 @@ public ObservableCollection<ProductionUnit> DisabledUnits { get; set; } = [];
     [ObservableProperty]
     private int costToCO2Ratio = 5;
 
+    [ObservableProperty]
+    private string? _selectedFilePath;
+
+    [ObservableProperty]
+    private string fileButtonText = "Select";
+
+
     public override string Title => "Configuration";
     public override Bitmap Icon => LoadAsset("config_icon.png");
 
-    public ConfigViewModel(AssetService assetService, OptimizationService optimizationService)
-    {
+    public ConfigViewModel(
+        AssetService assetService, 
+        OptimizationService optimizationService, 
+        Func<Task<string?>>? filePickerService = null){
+
         _assetService = assetService;
-        List<ProductionUnit> units = _assetService.GetProductionUnits();
-        foreach (ProductionUnit unit in units) {
-            ActiveUnits.Add(unit);
-        }
         _optimizationService = optimizationService;
+        _filePickerService = filePickerService;
+
+        var units = _assetService.GetProductionUnits();
+        if (units != null){
+
+            foreach (var unit in units){
+                ActiveUnits.Add(unit);
+            }
+        }
+    }
+
+    [RelayCommand]
+    private async Task SelectFileAsync(){
+
+        if (_filePickerService == null) return;
+
+        string? localPath = await _filePickerService.Invoke();
+
+        if (!string.IsNullOrEmpty(localPath))
+        {
+            SelectedFilePath = localPath; 
+            FileButtonText = System.IO.Path.GetFileName(localPath);
+        }
     }
 
     [RelayCommand]
@@ -51,4 +85,5 @@ public ObservableCollection<ProductionUnit> DisabledUnits { get; set; } = [];
             ActiveUnits.Add(unit);
         }
     }
+
 }
--- Presentation/Views/Config.axaml.cs
+++ Presentation/Views/Config.axaml.cs
@@ -1,5 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
 using HeatOptimization.Logic;
 using HeatOptimization.Presentation.ViewModels;
@@ -10,62 +9,40 @@ namespace HeatOptimization.Presentation.Views
 {
     public partial class Config : Window
     {
-        private string? _selectedFilePath;
-
-
-
         public Config()
         {
             InitializeComponent();
             if (App.AssetService != null && App.OptimizationService != null)
             {
-                DataContext = new ConfigViewModel(App.AssetService, App.OptimizationService);
+                DataContext = new ConfigViewModel(App.AssetService, App.OptimizationService, PickFileCrossPlatformAsync);
             }
         }
 
         public Config(AssetService assetService, OptimizationService optimizationService)
         {
             InitializeComponent();
-            DataContext = new ConfigViewModel(assetService, optimizationService);
+            DataContext = new ConfigViewModel(assetService, optimizationService, PickFileCrossPlatformAsync);
         }
 
-
-        private async void OnFileSelectClick(object sender, RoutedEventArgs e)
+        private async Task<string?> PickFileCrossPlatformAsync()
         {
-            var topLevel = TopLevel.GetTopLevel(this);
-            if (topLevel == null) return;
-
-            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            var files = await this.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
             {
-                Title = "Pick file wih hourly data",
-                AllowMultiple = false
+                Title = "Pick file with hourly data",
+                AllowMultiple = false,
+                FileTypeFilter = new[]
+                {
+                    new FilePickerFileType("Data Files") { Patterns = new[] { "*.csv", "*.xlsx", "*.json" } }
+                }
             });
 
-            if (files != null && files.Any())
-            {
-                var file = files[0];
-                _selectedFilePath = file.Path.LocalPath;
-
-                // This can be removed, but it shows name of the file
-                
-                FileSelectBtn.Content = file.Name;
-            }
-        }
-
-        private void OnOptimizeClick(object sender, RoutedEventArgs e)
-        {
-            var result = new ConfigResult
-            {
-                FilePath = _selectedFilePath,
-            };
-            
-            Close(result);
+            return files?.FirstOrDefault()?.Path.LocalPath;
         }
     }
 
     public class ConfigResult
     {
         public string? FilePath { get; set; }
-
+        public int MaintenanceHours { get; set; }
     }
 }
