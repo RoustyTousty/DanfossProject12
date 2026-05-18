f7f3be0d088a15e56a7582180f788599ea21a1f5
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2026-05-18 09:13:01 +0200

    removed unnecessary methods from AssetManager

--- Logic/AssetManager/AssetManager.cs
+++ Logic/AssetManager/AssetManager.cs
@@ -4,7 +4,6 @@ public class AssetManager
 {
     private IProductionUnitLibraryProvider _productionUnitLibraryProvider;
     private IHourlyChartProvider _hourlyChartProvider;
-    private List<IHourlyData> _hourlyData;
     private List<ProductionUnit> _productionUnits = [];
 
     public AssetManager (IHourlyChartProvider hourlyChartProvider, IProductionUnitLibraryProvider productionUnitLibraryProvider)
@@ -12,50 +11,15 @@ public class AssetManager
         _productionUnitLibraryProvider = productionUnitLibraryProvider;
         _hourlyChartProvider = hourlyChartProvider;
 
-        _hourlyData = _hourlyChartProvider.GetHourlyData();
         _productionUnits = _productionUnitLibraryProvider.GetProductionUnits();
     }
 
 
-
-    public ProductionUnit? GetProductionUnitByName(string name)
-    {
-        try
-        {
-            return _productionUnits.FirstOrDefault(productionUnit => productionUnit.Name == name);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Error retrieving production unit: {e.Message}");
-            return null;
-        }
-    }
-
-    
-    public IHourlyData? GetHourlyData(DateTime dateTime)
-    {
-        try
-        {
-            IHourlyData? data = _hourlyData.Find(x => x.TimeFrom < dateTime && x.TimeTo > dateTime);
-            if (data != null)
-            {
-                return data;
-            } else {
-                Console.WriteLine($"Error while retrieving hourly data at {dateTime}");
-                return null;
-            }
-        } catch (Exception e)
-        {
-            Console.WriteLine($"Error retrieving hourly data for {dateTime}: {e.Message}");
-            return null;
-        }
-    }
-
     public List<IHourlyData> GetHourlyDatas(string fpath)
     {
-        _hourlyData = _hourlyChartProvider.GetHourlyData(fpath);
-        return _hourlyData;
+        return _hourlyChartProvider.GetHourlyData(fpath);
     }
+    
     public List<ProductionUnit> GetProductionUnits()
     {
         return _productionUnits;
